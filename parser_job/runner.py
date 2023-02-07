ec0e48c51c24d7f0e6a2d635d656d399ae97bba8
Zhivlova <71148254+Zhivlova@users.noreply.github.com>
2023-02-07 19:29:59 +0300

    add runner

--- parser_job/runner.py
+++ parser_job/runner.py
@@ -3,3 +3,10 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 
+from parser_job.spiders.hh_ru import HhRuSpider
+
+if '__name__' == '__main__':
+    configure_logging()
+    settings = get_project_settings()
+    runner = CrawlerRunner(settings)
+    runner.crawl(HhRuSpider)
